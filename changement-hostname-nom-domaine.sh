55aed78ea142e2de3faf371297c8f3e4d6216684
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-04-02 07:03:29 +0200

    Correction [changement-hostname-nom-domaine.sh] : des commandes avaient été copiés/colés en double  par erreur, introduissant une erreur de configuration réseau.

--- changement-hostname-nom-domaine.sh
+++ changement-hostname-nom-domaine.sh
@@ -143,13 +143,11 @@ export FICHIERTEMP=./config-int-reseau.girofle
  # ================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 	> /etc/sysconfig/network-scripts/ifcfg-enp0s3
 rm -f $FICHIERTEMP
 sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s3 >> $FICHIERTEMP
-sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s3 >> $FICHIERTEMP
 echo "HOSTNAME=kytes-ssh.io" >> $FICHIERTEMP
 sudo rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s3
 sudo cp -f $FICHIERTEMP /etc/sysconfig/network-scripts/ifcfg-enp0s3
  # ================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 	> /etc/sysconfig/network-scripts/ifcfg-enp0s8
 rm -f $FICHIERTEMP
-sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s8 >> $FICHIERTEMP
 sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s8 >> $FICHIERTEMP
 echo "HOSTNAME=kytes-alt1.io" >> $FICHIERTEMP
 sudo rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s8
@@ -157,13 +155,11 @@ sudo cp -f $FICHIERTEMP /etc/sysconfig/network-scripts/ifcfg-enp0s8
  # ================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 	> /etc/sysconfig/network-scripts/ifcfg-enp0s9
 rm -f $FICHIERTEMP
 sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s9 >> $FICHIERTEMP
-sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s9 >> $FICHIERTEMP
 echo "HOSTNAME=kytes-alt2.io" >> $FICHIERTEMP
 sudo rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s9
 sudo cp -f $FICHIERTEMP /etc/sysconfig/network-scripts/ifcfg-enp0s9
  # ================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 	> /etc/sysconfig/network-scripts/ifcfg-enp0s10
 rm -f $FICHIERTEMP
-sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s10 >> $FICHIERTEMP
 sudo cat /etc/sysconfig/network-scripts/ifcfg-enp0s10 >> $FICHIERTEMP
 echo "HOSTNAME=$NOMDEDOMAINE" >> $FICHIERTEMP
 sudo rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s10
